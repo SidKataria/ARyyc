c5e680fa28aa7b66cf36f28b4fe14b6df61e17ad
Siddharth Kataria <siddharth.kataria@ucalgary.ca>
2019-11-13 12:00:31 -0700

    Initial Commit

---
+++ ARyyc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ARyyc
+//
+//  Created by Siddharth Kataria on 2019-11-13.
+//  Copyright © 2019 Siddharth Kataria. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
